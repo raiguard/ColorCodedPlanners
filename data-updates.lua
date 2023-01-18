8d1bc6ddb00ec19656bb8231b1100fd972b0560f
Caleb Heuer <ch@raiguard.me>
2023-01-18 13:30:51 -0700

    Add color for reverse-upgrade

--- data-updates.lua
+++ data-updates.lua
@@ -1,42 +1,44 @@
 local changes = {
-  ["blueprint"] = {
-    ["blueprint"] = {
-      selection_cursor_box_type = "electricity",
-      alt_selection_cursor_box_type = "electricity"
-    }
-  },
-  ["copy-paste-tool"] = {
-    ["cut-paste-tool"] = {
-      selection_cursor_box_type = "not-allowed",
-      alt_selection_cursor_box_type = "not-allowed"
-    },
-    ["copy-paste-tool"] = {
-      selection_cursor_box_type = "electricity",
-      alt_selection_cursor_box_type = "electricity"
-    }
-  },
-  ["deconstruction-item"] = {
-    ["deconstruction-planner"] = {
-      selection_cursor_box_type = "not-allowed",
-      alt_selection_cursor_box_type = "not-allowed"
-    }
-  },
-  ["upgrade-item"] = {
-    ["upgrade-planner"] = {
-      selection_cursor_box_type = "copy",
-      alt_selection_cursor_box_type = "copy"
-    }
-  }
+	["blueprint"] = {
+		["blueprint"] = {
+			selection_cursor_box_type = "electricity",
+			alt_selection_cursor_box_type = "electricity",
+		},
+	},
+	["copy-paste-tool"] = {
+		["cut-paste-tool"] = {
+			selection_cursor_box_type = "not-allowed",
+			alt_selection_cursor_box_type = "not-allowed",
+		},
+		["copy-paste-tool"] = {
+			selection_cursor_box_type = "electricity",
+			alt_selection_cursor_box_type = "electricity",
+		},
+	},
+	["deconstruction-item"] = {
+		["deconstruction-planner"] = {
+			selection_cursor_box_type = "not-allowed",
+			alt_selection_cursor_box_type = "not-allowed",
+		},
+	},
+	["upgrade-item"] = {
+		["upgrade-planner"] = {
+			selection_cursor_box_type = "copy",
+			alt_selection_cursor_box_type = "copy",
+			reverse_selection_cursor_box_type = "entity",
+			alt_reverse_selection_cursor_box_type = "entity",
+		},
+	},
 }
 
 for category, items in pairs(changes) do
-  local category_data = data.raw[category]
-  for name, mods in pairs(items) do
-    local item = category_data[name]
-    for k, v in pairs(mods) do
-      item[k] = v
-    end
-  end
+	local category_data = data.raw[category]
+	for name, mods in pairs(items) do
+		local item = category_data[name]
+		for k, v in pairs(mods) do
+			item[k] = v
+		end
+	end
 end
 
 data.raw["utility-sprites"]["default"]["upgrade_mark"].filename = "__ColorCodedPlanners__/graphics/upgrade.png"
